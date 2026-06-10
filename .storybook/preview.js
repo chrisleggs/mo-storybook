4744a9348727dabbb4d3c764169d0ead4fb6de71
Chris Legg <christopherlegg@me.com>
2026-06-10 18:28:35 +0000

    Configure a11y ruleset: WCAG 2.0–2.2 A/AA + best-practice

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,22 @@
 /** @type { import('@storybook/html').Preview } */
 const preview = {
   parameters: {
+    a11y: {
+      options: {
+        runOnly: {
+          type: 'tag',
+          values: [
+            'wcag2a',
+            'wcag2aa',
+            'wcag21a',
+            'wcag21aa',
+            'wcag22a',
+            'wcag22aa',
+            'best-practice',
+          ],
+        },
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
